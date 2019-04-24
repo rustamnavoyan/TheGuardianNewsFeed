8b4f6906ef3bdbc880372f5216afddd0b589864e
Rustam Navoyan <rustam.navoyan@gmail.com>
2019-04-24 18:07:41 +0400

    Show loading progress bar at the end of the list while loading

--- app/src/main/java/com/rustamnavoyan/theguardiannewsfeed/MainFragment.java
+++ app/src/main/java/com/rustamnavoyan/theguardiannewsfeed/MainFragment.java
@@ -78,6 +78,7 @@ public class MainFragment extends Fragment implements
 
                 // start loading articles if there are 2 items to reach the last loaded item
                 if (totalItemCount <= (lastVisibleItem + 2)) {
+                    adapter.setLoading();
                     loadArticles(adapter);
                 }
             }
--- app/src/main/java/com/rustamnavoyan/theguardiannewsfeed/adapters/ArticleListAdapter.java
+++ app/src/main/java/com/rustamnavoyan/theguardiannewsfeed/adapters/ArticleListAdapter.java
@@ -16,13 +16,19 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.ArticleViewHolder> {
+public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.AbstractViewHolder> {
 
     public interface OnItemClickListener {
         void onItemClicked(ArticleItem article);
     }
 
-    static class ArticleViewHolder extends RecyclerView.ViewHolder {
+    static abstract class AbstractViewHolder extends RecyclerView.ViewHolder {
+        AbstractViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    static class ArticleViewHolder extends AbstractViewHolder {
         private ImageView mThumbnail;
         private TextView mTitle;
         private TextView mCategory;
@@ -36,10 +42,20 @@ public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.
         }
     }
 
+    static class ProgressViewHolder extends AbstractViewHolder {
+        ProgressViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    private static final int VIEW_TYPE_ITEM = 0;
+    private static final int VIEW_TYPE_PROGRESS = 1;
+
     private List<ArticleItem> mArticleItemList = new ArrayList<>();
     private OnItemClickListener mItemClickListener;
     private boolean mPinned;
     private int mScreenWidth;
+    private boolean mLoading;
 
     public ArticleListAdapter(boolean pinned, int screenWidth, OnItemClickListener itemClickListener) {
         mPinned = pinned;
@@ -47,7 +63,25 @@ public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.
         mItemClickListener = itemClickListener;
     }
 
+    public void setLoading() {
+        if (mLoading) {
+            return;
+        }
+        mLoading = true;
+        notifyItemInserted(mArticleItemList.size());
+    }
+
+    private void setLoaded() {
+        if (!mLoading) {
+            return;
+        }
+        mLoading = false;
+        notifyItemRemoved(mArticleItemList.size());
+    }
+
     public void addArticleList(List<ArticleItem> articleItemList) {
+        setLoaded();
+
         int start = mArticleItemList.size();
         mArticleItemList.addAll(articleItemList);
         notifyItemRangeInserted(start, articleItemList.size());
@@ -58,43 +92,64 @@ public class ArticleListAdapter extends RecyclerView.Adapter<ArticleListAdapter.
     }
 
     public void setArticleList(List<ArticleItem> articleList) {
+        setLoaded();
         mArticleItemList = articleList;
         notifyDataSetChanged();
     }
 
     public void clearArticles() {
+        setLoaded();
         mArticleItemList.clear();
         notifyDataSetChanged();
     }
 
     @NonNull
     @Override
-    public ArticleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_item_article, parent, false);
-        ArticleViewHolder holder = new ArticleViewHolder(view);
-        if (mPinned) {
-            view.setBackgroundColor(parent.getContext().getResources().getColor(R.color.pinned_bg_color));
-            ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
-            layoutParams.width = (int) (mScreenWidth * 0.8);
+    public AbstractViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_ITEM) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item_article, parent, false);
+            ArticleViewHolder holder = new ArticleViewHolder(view);
+            if (mPinned) {
+                view.setBackgroundColor(parent.getContext().getResources().getColor(R.color.pinned_bg_color));
+                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+                layoutParams.width = (int) (mScreenWidth * 0.8);
+            }
+            view.setOnClickListener(v -> {
+                mItemClickListener.onItemClicked(mArticleItemList.get(holder.getAdapterPosition()));
+            });
+            return holder;
         }
-        view.setOnClickListener(v -> {
-            mItemClickListener.onItemClicked(mArticleItemList.get(holder.getAdapterPosition()));
-        });
 
-        return holder;
+        return new ProgressViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_progress, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ArticleViewHolder holder, int position) {
-        ArticleItem articleItem = mArticleItemList.get(position);
-        holder.mTitle.setText(articleItem.getTitle());
-        holder.mCategory.setText(articleItem.getCategory());
-        Picasso.get().load(articleItem.getThumbnailUrl()).into(holder.mThumbnail);
+    public void onBindViewHolder(@NonNull AbstractViewHolder holder, int position) {
+        if (holder instanceof ArticleViewHolder) {
+            ArticleViewHolder articleViewHolder = (ArticleViewHolder) holder;
+            ArticleItem articleItem = mArticleItemList.get(position);
+            articleViewHolder.mTitle.setText(articleItem.getTitle());
+            articleViewHolder.mCategory.setText(articleItem.getCategory());
+            Picasso.get().load(articleItem.getThumbnailUrl()).into(articleViewHolder.mThumbnail);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position < mArticleItemList.size()) {
+            return VIEW_TYPE_ITEM;
+        }
+        return VIEW_TYPE_PROGRESS;
     }
 
     @Override
     public int getItemCount() {
-        return mArticleItemList.size();
+        if (mArticleItemList.isEmpty()) {
+            return 0;
+        }
+        // 1 for progress
+        return mPinned || !mLoading ? mArticleItemList.size() : mArticleItemList.size() + 1;
     }
 }
