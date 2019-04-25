d6bcba869bba2034eb802ce8004960ebab9332ee
Rustam Navoyan <rustam.navoyan@gmail.com>
2019-04-25 15:58:32 +0400

    Reschedule periodically checking for new or updated articles after device restarting

---
+++ app/src/main/java/com/rustamnavoyan/theguardiannewsfeed/manage/StartupReceiver.java
@@ -0,0 +1,12 @@
+package com.rustamnavoyan.theguardiannewsfeed.manage;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartupReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        PeriodicDownloadManager.schedule(context);
+    }
+}
